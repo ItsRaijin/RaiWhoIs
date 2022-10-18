f25db49c8aa6bfa5d6c0efb1f20ea70c9f391069
Habib <habibleton@gmail.com>
2022-10-19 03:41:23 +1100

    changes
    ege

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useEffect } from "react";
+import pfp from './images/profile.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    let hidden = document.querySelectorAll('.reveal')
+    hidden.forEach((item) => observer.observe(item));
+  }, [])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="heading">
+
+        <button className='name button'>Habibullah Saleem</button>
+
+        <div className='navbar'>
+          <button className='nav button'>Home</button>
+          <button className='nav button'>Portfolio</button>
+          <button className='nav button'>About</button>
+        </div>
+
+        <button className='contact button'>Contact Me</button>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='home'>
+
+        <div className='leftHome'>
+          <img className='pfp' src={pfp}></img>
+          <p className='title'>Developer +</p>
+        </div>
+
+        <div className='rightHome'>
+          <p className='intro'>Hi There, I'm Habib</p>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      <div className="container">
+        
+      </div>
+
+
+      <div className="container">
+        
+      </div>
+
+      <div className="container">
+        
+      </div>
+
     </div>
   )
+
+  
 }
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active');
+    }
+  });
+});
+
+
+
+/*window.addEventListener("scroll", reveal);*/
+
+// To check the scroll position on page load
+//reveal();
 
 export default App
